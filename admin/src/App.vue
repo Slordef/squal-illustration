72ac00a3110ec2a150b4a3404a515d6786f280a5
Maximilien Valenzano <slordef.vikamet@gmail.com>
2022-02-11 23:55:58 +0100

    Change Express to Fastify

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <Header />
   <router-view/>
 </template>
 
@@ -28,3 +25,9 @@ nav {
   }
 }
 </style>
+<script>
+import Header from '@/components/HeaderNavigation'
+export default {
+  components: { Header }
+}
+</script>
