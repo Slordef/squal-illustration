41593a762346d91fbc494554152ea6ae661670c8
Maximilien Valenzano <slordef.vikamet@gmail.com>
2022-02-24 17:00:10 +0100

    Type Axios Api

--- client/src/store/api.ts
+++ client/src/store/api.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosResponse, AxiosPromise } from 'axios'
 
 class Api {
-  get<T> (url: string): AxiosPromise<AxiosResponse<T>> { return axios.get(`/api${url}`).then(r => r.data) }
-  post<T> (url: string, data: unknown): AxiosPromise<AxiosResponse<T>> { return axios.post(`/api${url}`, data).then(r => r.data) }
+  get<T> (url: string): AxiosPromise<T> { return axios.get(`/api${url}`).then(r => r.data) }
+  post<T> (url: string, data: unknown): AxiosPromise<T> { return axios.post(`/api${url}`, data).then(r => r.data) }
 }
 
 export const api = new Api()
